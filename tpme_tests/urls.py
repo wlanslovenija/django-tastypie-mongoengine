5f54ee4e76d88b1004a223b4c0a4ee39d1c7b64e
Matevž <matevz.mihalic@gmail.com>
2012-03-21 15:00:52 +0100

    Basic structure.

---
+++ tpme_tests/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tpme_tests.views.home', name='home'),
+    # url(r'^tpme_tests/', include('tpme_tests.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
