887714ac5fb658ff441077dbfec2e2af64af1a1e
Mitar <mitar.git@tnode.com>
2013-10-06 03:46:04 -0700

    Support Django 1.4.

---
+++ tastypie_mongoengine/__init__.py
@@ -0,0 +1,9 @@
+try:
+    import django.db.models.constants.LOOKUP_SEP
+except ImportError:
+    # To support Django 1.4 we move to location where Django 1.5+ has constants
+    import sys
+    from django.db.models.sql import constants
+    import django.db.models
+    django.db.models.constants = constants
+    sys.modules['django.db.models.constants'] = django.db.models.constants
